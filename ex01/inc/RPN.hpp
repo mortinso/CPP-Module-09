dfb770850632b66901193c1332e1b391028732ab
mortinso <89762267+mortinso@users.noreply.github.com>
2024-11-20 18:12:40 +0000

    ex01: Added colors.hpp

--- ex01/inc/RPN.hpp
+++ ex01/inc/RPN.hpp
@@ -6,7 +6,7 @@
 /*   By: mortins- <mortins-@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/28 12:23:39 by mortins-          #+#    #+#             */
-/*   Updated: 2024/11/19 16:47:19 by mortins-         ###   ########.fr       */
+/*   Updated: 2024/11/20 18:11:51 by mortins-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,18 +18,7 @@
 # include <algorithm>
 # include <sstream>
 # include <list>
-
-# define BOLD "\e[1m"
-# define ITALIC "\e[3m"
-# define UNDERLINE "\e[4m"
-# define RED "\e[91m"
-# define GREEN "\e[92m"
-# define YELLOW "\e[93m"
-# define BLUE "\e[94m"
-# define PURPLE "\e[95m"
-# define CYAN "\e[96m"
-# define INVERT "\e[90m\e[107m"
-# define RESET "\e[0m"
+# include "colors.hpp"
 
 class RPN {
 	private:
--- ex01/src/main.cpp
+++ ex01/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mortins- <mortins-@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/28 12:23:37 by mortins-          #+#    #+#             */
-/*   Updated: 2024/11/20 18:03:58 by mortins-         ###   ########.fr       */
+/*   Updated: 2024/11/20 18:11:42 by mortins-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,14 @@
 
 int	main( int argc, char **argv ) {
 	if (argc != 2) {
-		std::cerr << "Wrong number of arguments. Usage:" << std::endl << "./RPN <expression>" << std::endl;
+		std::cerr << RED_H << "Wrong number of arguments. Usage:" << std::endl << "./RPN <expression>" << std::endl;
 		return 1;
 	}
 	try {
 		RPN	rpn;
 		rpn.doRPN(argv[1]);
 	}
-	catch (const std::exception &e) { std::cerr << RED << e.what() << RESET << std::endl;}
+	catch (const std::exception &e) { std::cerr << RED_H << e.what() << RESET << std::endl;}
 }
 
 // to compare results, visit https://paodayag.dev/reverse-polish-notation-js-parser/
